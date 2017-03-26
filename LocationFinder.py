7773423e1bbcf1b10992de104d7c9e6b8061343c
Scott <Scott@Standring.com>
2017-03-26 14:04:29 -0700

    Got the list for the first page
    Need to figure out a way to get multiple pages, and begin making the txt
    file more complex (col.)

--- LocationFinder.py
+++ LocationFinder.py
@@ -1,10 +1,26 @@
 #python3
 #https://www.alltrails.com/us/california/backpacking scraping for locations
 
-from bs4 import BeautifulSoup
-import urllib
-import requests
+import bs4 as bs
+import urllib.request
+import csv
+import lxml
 
-trail_soup = BeautifulSoup(open("https://www.alltrails.com/us/california/backpacking"))
-print (soup.prettify())
-trail_sauce = trail_soup.fina_all('a')
+#def trail_info():
+trail_sauce = urllib.request.urlopen('https://www.alltrails.com/us/california/backpacking').read()
+trail_soup = bs.BeautifulSoup(trail_sauce, 'lxml')
+trail_data = trail_soup.find_all('a', class_='link mobile-block')
+
+
+print(trail_data)
+trail_list=[]
+
+for a in trail_data:
+    trail_name = (a.text)
+    trails=[trail_name]
+    trail_list.append(trails)
+
+with open('Trail_list.csv', 'w') as file:
+    csv_writer = csv.writer(file)
+    for row in trail_list:
+        csv_writer.writerow(row)
