421dc425d81325929049da6246b51104c18cabcd
Sarulian <tedjamesrobinson@gmail.com>
2017-03-26 11:59:00 -0700

    basic functionality of gps from address

---
+++ gMaps.py
@@ -0,0 +1,21 @@
+import googlemaps
+from datetime import datetime
+import pprint, json
+
+
+# Get API key from file stored in GithubPrivates folder outside of Github repos
+
+with open("O:\\Users\\Theodore\\Documents\\GithubPrivates\\GoogleMapsAPIKeys.txt","r") as myfile:
+	APIKey = myfile.read()
+
+gmaps = googlemaps.Client(key=APIKey)
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+print(json.dumps(geocode_result, indent=4))
+print("\n########################################################################\n")
+print("Latitude:")
+print(geocode_result[0]['geometry']['location']['lat'])
+print("Longitude:")
+print(geocode_result[0]['geometry']['location']['lng'])
