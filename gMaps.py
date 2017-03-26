a719598d226eb43f711102b014938c0b4f6e8644
Sarulian <tedjamesrobinson@gmail.com>
2017-03-26 12:23:05 -0700

    put everything into functions

--- gMaps.py
+++ gMaps.py
@@ -4,18 +4,43 @@ import pprint, json, os
 
 
 # Get API key from file stored in GithubPrivates folder outside of Github repos
+def get_api_key():
 
-with open(os.getcwd() + "\\..\\..\\GithubPrivates\\googleapikey.txt","r") as myfile:
-	APIKey = myfile.read()
+	with open(os.getcwd() + "\\..\\..\\GithubPrivates\\googleapikey.txt","r") as myfile:
+		api_key = myfile.read()
 
-gmaps = googlemaps.Client(key=APIKey)
+	return api_key
 
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
-print(json.dumps(geocode_result, indent=4))
-print("\n########################################################################\n")
-print("Latitude:")
-print(geocode_result[0]['geometry']['location']['lat'])
-print("Longitude:")
-print(geocode_result[0]['geometry']['location']['lng'])
+def get_gps_coords(location, api_key):
+
+	gmaps = googlemaps.Client(key=api_key)
+
+	# Geocoding an address
+	geocode_result = gmaps.geocode(location)
+
+	lat = geocode_result[0]['geometry']['location']['lat']
+	lng = geocode_result[0]['geometry']['location']['lng']
+
+	print(json.dumps(geocode_result, indent=4))
+	print("\n########################################################################\n")
+	print("Latitude:")
+	print(geocode_result[0]['geometry']['location']['lat'])
+	print("Longitude:")
+	print(geocode_result[0]['geometry']['location']['lng'])
+
+	return lat, lng
+
+
+def main():
+
+	api_key = get_api_key()
+
+	location = input("What location are you looking for?:")
+
+	get_gps_coords(location, api_key)
+
+
+if __name__ == "__main__":
+	main()
+	
