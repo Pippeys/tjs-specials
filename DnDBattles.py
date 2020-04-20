17111a79de6c11abccb657a6dbb44b1e6da83084
Sarulian <tedjamesrobinson@gmail.com>
2020-04-19 20:23:32 -0700

    Enabling crits and advantage to attack rolls

--- DnDBattles.py
+++ DnDBattles.py
@@ -3,26 +3,37 @@ import re
 import requests
 from pprint import pprint
 
+
 class Roll:
+
     def __init__(self, name, dice_string, mod):
         self.name = name
         self.dice = []
         self.parse_dice_string(dice_string)
         self.mod = mod
+        self.max = sum([die_roll[1] for die_roll in self.dice]) + self.mod
+        self.critical = False
 
     def parse_dice_string(self, dice_string):
         roll_strings = dice_string.split("+")
         for roll_string in roll_strings:
             self.dice.append([int(num) for num in roll_string.split("d")])
 
-    def add_die(self, num, size):
-        self.dice.append([num, size])
+    def make_critical(self):
+        self.critical = True
 
     def roll(self):
         total = self.mod
-        for die_roll in self.dice:
+        if self.critical:
+            dice = [[die_roll[0]*2, die_roll[1]] for die_roll in self.dice]
+        else:
+            dice = self.dice
+
+        for die_roll in dice:
             for die in range(die_roll[0]):
                 total += random.randint(1, die_roll[1])
+
+        self.critical = False
         return total
 
     def __str__(self):
@@ -31,30 +42,49 @@ class Roll:
         dice_string += "+{}".format(self.mod)
         return "{}: {} result = {}".format(self.name, dice_string, result)
 
-        class Attack:
 
-            def __init__(self, name, to_hit_mod, damage_string, damage_mod, damage_type):
-                self.name = name
-                self.to_hit = Roll(
-                        "{} to hit".format(self.name),
-                        "1d20",
-                        to_hit_mod
-                    )
-                self.for_damage = Roll(
-                        "{} for damage".format(self.name),
-                        damage_string,
-                        damage_mod
-                    )
+class Attack:
 
+    def __init__(self, name, to_hit_mod, damage_string, damage_mod, damage_type, has_advantage=False):
+        self.name = name
+        self.to_hit_mod = to_hit_mod
+        self.damage_string = damage_string
+        self.damage_mod = damage_mod
+        self.to_hit = Roll(
+                "{} to hit".format(self.name),
+                "1d20",
+                to_hit_mod
+            )
+        self.for_damage = Roll(
+                "{} for damage".format(self.name),
+                damage_string,
+                damage_mod
+            )
+        self.has_advantage = has_advantage
 
-            def roll_to_hit(self):
-                return self.to_hit.roll()
+    def give_advantage(self):
+        self.has_advantage = True
 
-            def roll_for_damage(self):
-                return self.for_damage.roll()
+    def roll_to_hit(self):
+        to_hit_result = self.to_hit.roll()
 
-            def __str__(self):
-                return "Attack Type: {} \nTo hit: {} \nDamage: {}".format(self.name, self.roll_to_hit(), self.roll_for_damage())
+        if self.has_advantage:
+            second_roll = self.to_hit.roll()
+            to_hit_result = max([to_hit_result, second_roll])
+
+        if to_hit_result == self.to_hit.max:
+            self.for_damage.make_critical()
+        return to_hit_result
+
+    def roll_for_damage(self):
+        return self.for_damage.roll()
+
+    def __str__(self):
+        return "{}: To hit - 1d20+{}, Damage - {}+{}".format(self.name, self.to_hit_mod, self.damage_string, self.damage_mod)
+
+    def attack(self):
+        return self.roll_to_hit(), self.roll_for_damage()
+
 
 class Monster:
     def __init__(self,Name):
@@ -82,10 +112,14 @@ class Monster:
             except KeyError:
                 continue
 
+
 class Alden:
-    def __init__(self,AC,Health)
+    def __init__(self,AC,Health):
+        pass
+
 
 def Battle (Player,Monster,Player_Attack,Monster_Attack):
+    pass
 
 
 def main():
